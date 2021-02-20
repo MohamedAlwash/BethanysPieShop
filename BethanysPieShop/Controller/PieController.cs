2c1c95f3586aa99f5810b847307307f00f51a3c3
Mohamed Alwash <18046533@student.hhs.nl>
2021-02-20 16:30:46 +0100

    Added view

---
+++ BethanysPieShop/Controller/PieController.cs
@@ -0,0 +1,27 @@
+﻿using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controller
+{
+    public class PieController : ControllerBase
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IPieRepository _pieRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            PiesListViewModel piesListViewModel = new PiesListViewModel();
+            piesListViewModel.Pies = _pieRepository.AllPies;
+
+            piesListViewModel.CurrentCategory = "Cheese cakes";
+            return view(piesListViewModel);
+        }
+    }
+}
--- BethanysPieShop/Models/MockCategoryRepository.cs
+++ BethanysPieShop/Models/MockCategoryRepository.cs
@@ -8,8 +8,7 @@ namespace BethanysPieShop.Models
         {
             new Category {CategoryId = 1, CategoryName = "Fruit pies", Description = "All-fruity pies"},
             new Category {CategoryId = 2, CategoryName = "Cheese cakes", Description = "Cheesy all the way"},
-            new Category
-                {CategoryId = 3, CategoryName = "Seasonal pies", Description = "Get in the mood for a seasonal pie"}
+            new Category {CategoryId = 3, CategoryName = "Seasonal pies", Description = "Get in the mood for a seasonal pie"}
         };
     }
 }
